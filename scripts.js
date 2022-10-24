93ab6a56df424461fb9dfb5ef24c55462e159a28
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-10-24 15:09:33 -0400

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const family = [
+    {
+        dad: true,
+        name: 'Joe',
+        job: 'mechanic',
+    },
+    {
+        kid1: 'Mia',
+        pet: 'fish'
+    }
+
+
+]
+console.log('hey class here is my fam', family)
+
+document.getElementById('contact').style.color = 'purple'
+
+const handleColorChange = () => {
+
+    console.log('changing color')
+}
+
+const handleAlert = () => {
+
+    alert('Alerting the class')
+}
